fd20f15f79396cc946756ca8852ed9a3173a68d1
AndresdPM <63738265+AndresdPM@users.noreply.github.com>
2021-06-02 22:22:54 -0400

    Update install_GaiaHub.py

--- install_GaiaHub.py
+++ install_GaiaHub.py
@@ -286,6 +286,8 @@ def installation():
             make_alias(installation_folder, master)
             print('You can now run %s from anywere in your computer by typing "%s" in the terminal.\n'%(master, master.lower()))
             print('Try "%s --help" to learn how to execute it.\n'%master.lower())
+            print('(You may need to open a new terminal)')
+
       else:
             print('No modification was made to .bash_profile.\n')
             print('To run %s, you will have to execute the python script located here:\n'%master)
